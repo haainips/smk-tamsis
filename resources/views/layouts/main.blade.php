3445f38592cb501b036854beab4ab2cd732cfb43
Fata Allam Hanifa <fatahanif@students.amikom.ac.id>
2025-04-24 15:59:24 +0700

    debug tailwind build process 5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,33 @@
     <title>@yield('title')</title>
     <link rel="icon" href="{{ asset('images/icons/logo1.png') }}" type="image/png">
     @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: "#02807F",
+                        secondary: "#015f5f",
+                        slight: "#359999",
+                        sign: "#DE8B2C",
+                    },
+                    fontFamily: {
+                        title: ["Racing Sans One", "cursive"], // Untuk kelas `font-title`
+                        subtitle: ["Poppins", "sans-serif"], // Untuk kelas `font-subtitle`
+                    },
+                    dropShadow: {
+                        default: "0 4px 4px rgba(0, 0, 0, 0.25)",
+                    }
+                }
+            }
+        }
+    </script>
 </head>
 
 <body class="flex flex-col min-h-screen scroll-smooth">
